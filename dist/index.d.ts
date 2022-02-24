d242d7f77a04471b76b303d163bbdc693ab11838
Guilherme Tramontano <tramontano.guiherme@yahoo.com.br>
2022-02-24 13:48:56 -0300

    chore: moving d.ts to project root

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import ReactAdapter, { InitReactAdapter } from './Adapters/ReactAdapter';
-export * from './Counter';
-export declare const initReact: (initOptions: InitReactAdapter) => ReactAdapter;
---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import ReactAdapter, { InitReactAdapter } from './dist/Adapters/ReactAdapter';
+export * from './dist/Counter';
+export declare const initReact: (initOptions: InitReactAdapter) => ReactAdapter;
